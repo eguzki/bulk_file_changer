cf59801b98ac038bb7ec4ef2e5e9f901fadc4b12
Eguzki Astiz Lezaun <eastizle@redhat.com>
2024-10-07 00:08:05 +0200

    image extensions filter

--- src/stats.rs
+++ src/stats.rs
@@ -1,18 +1,21 @@
 use crate::datetime_tag_parser;
 use crate::datetime_tag_writer;
 use serde::Serialize;
+use std::ffi::OsStr;
 use walkdir::DirEntry;
 
 // Parsing stats
 #[derive(Serialize, PartialEq, Debug)]
 pub struct Stats {
     num_files: u32,
+    num_files_not_images: u32,
     num_files_with_datetime_tag: u32,
     num_files_failed_tag_parsing: u32,
     num_files_missing_datetime_tag: u32,
     num_files_failed_filename_parsing: u32,
     num_files_successfully_tagged: u32,
     num_files_failed_tagging: u32,
+    filenames_not_images: Vec<String>,
     filenames_tag_unparseable: Vec<String>,
     filenames_name_unparseable: Vec<String>,
     filenames_name_untaggeable: Vec<String>,
@@ -22,12 +25,14 @@ impl Stats {
     fn new() -> Stats {
         Stats {
             num_files: 0,
+            num_files_not_images: 0,
             num_files_with_datetime_tag: 0,
             num_files_failed_tag_parsing: 0,
             num_files_missing_datetime_tag: 0,
             num_files_failed_filename_parsing: 0,
             num_files_successfully_tagged: 0,
             num_files_failed_tagging: 0,
+            filenames_not_images: Vec::new(),
             filenames_tag_unparseable: Vec::new(),
             filenames_name_unparseable: Vec::new(),
             filenames_name_untaggeable: Vec::new(),
@@ -43,6 +48,14 @@ impl FromIterator<DirEntry> for Stats {
             //full path: i.path().display()
             //filename: i.file_name().to_str()
             s.num_files += 1;
+
+            if let Some(ext) = i.path().extension().and_then(OsStr::to_str) {
+                if !["jpeg", "jpg", "png"].contains(&ext) {
+                    s.num_files_not_images += 1;
+                    s.filenames_not_images.push(i.path().display().to_string());
+                    continue;
+                }
+            }
 
             log::debug!("{} {}", i.path().display(), i.file_name().to_str().unwrap());
 
